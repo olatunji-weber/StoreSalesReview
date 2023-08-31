4cd4923b9f8823ca92ba447feb5eeb5fbad1adb4
Olatunji Olayinka <olatunji.weber@gmail.com>
2023-08-31 21:09:16 +0200

    All the questions have been answered using functions and looping constructs...

--- sales_review.py
+++ sales_review.py
@@ -15,16 +15,48 @@ def gross_average_price(price_list):
 
 def new_price_list(reduce_by):
     # create a new price list that reduces the old prices by $ 5
-    pass
+    new_list = []
+    for price in prices:
+        new_list.append(price - reduce_by)
+    return new_list
+
+# prices = [30, 25, 40, 20, 20, 35, 45, 50, 35]
+# last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
 
 
-def gross_revenue():
+def gross_revenue(sales_list):
     # calculate the total revenue generated from the products
-    pass
+    total = 0
+    for i in range(len(prices)):
+        total += prices[i] * last_week[i]
+    return total
 
 
 def average_daily_revenue():
     # calculate the average daily revenue generated from the products
-    pass
+    for i in range(len(products)):
+        print(f"{products[i]} : {(prices[i] * last_week[i]) / 7}")
+
+
+print(
+    f"The Average of the prices of all products in the store is: {gross_average_price(prices)}")
+
+print(f"The new price list is: {new_price_list(5)}")
+
+print(f"The Total Sales is: {gross_revenue(last_week)}")
+
+print("****" * 5)
+print("****" * 5)
+print()
+print("The Average daily revenue generated from the products: ")
+average_daily_revenue()
+print()
+print("****" * 5)
+print("****" * 5)
 
 # based on the new prices, which products are less than $ 30
+
+new_list = new_price_list(5)
+for i in range(len(new_list)):
+    if new_list[i] < 30:
+        print(f"Product {products[i]}'s new price is less than $30")
